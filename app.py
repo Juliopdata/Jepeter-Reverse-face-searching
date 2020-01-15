d7351c4747761b7da8cf415351fc009691289ef7
Julio Porto <juliopdata@gmail.com>
2020-01-15 18:52:53 +0100

    hiring

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import tensorflow as tf
-from flask import Flask, render_template, request, send_from_directory
-from werkzeug import secure_filename
 import os
-from recomendator import recomender, demonator, clarator
 import numpy as np
 import pandas as pd
+
+from src.recomendator import recomender, clarator, demonator
+from flask import Flask, render_template, request, send_from_directory
+from werkzeug.utils import secure_filename
+
+
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+#Data import
 data = pd.read_csv('./data/photoDf.csv')
-vectors = np.load('vectorsnp.npy')
+vectors = np.load('./data/vectorsnp.npy')
 names = [f for f in data.Path]
 
 # instancia del objeto Flask
 app = Flask(__name__)
 
-# Carpeta static alternativa
-# Carpeta de subida
 
 # Custom static data
 @app.route('/upload/<path:path>')
@@ -26,9 +29,21 @@ def custom_static(path):
 
 @app.route("/")
 def upload_file():
-    # renderiamos la plantilla "formulario.html"
+    # renderizamos la plantilla "formulario.html"
     return render_template('index.html')
 
+
+'''@app.route("/upload", methods=['POST'])
+def uploader():
+    if request.method == 'POST':
+        # obtenemos el archivo del input "archivo"
+        f = request.files['archivo']
+        filename = secure_filename(f.filename)
+        print("--------postsecure-----------")
+        # Guardamos el archivo en el directorio "Archivos PDF"
+        f.save(os.path.join('./uploadphotos', filename))
+        res = recomender(vectors, names, './uploadphotos/{}'.format(filename))
+        return render_template('resultado.html', filename=res)'''
 
 @app.route("/upload", methods=['POST'])
 def uploader():
@@ -51,8 +66,8 @@ def uploader():
             res = recomender(vectors, names, './uploadphotos/{}'.format(filename))
             return render_template('resultado.html', filename=res)
 
-    # Iniciamos la aplicación
-    app.run(debug=True)
+# Iniciamos la aplicación
+app.run(debug=True)
 
 
 if __name__ == "__main__":
